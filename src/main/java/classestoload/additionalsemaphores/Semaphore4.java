d9898350cbbaf9b213af06589cfcbfae99e084e1
dnedvetsky <d.nedved@tut.by>
2016-08-09 21:18:03 +0300

    Updated implementation

---
+++ src/main/java/classestoload/additionalsemaphores/Semaphore4.java
@@ -0,0 +1,10 @@
+package classestoload.additionalsemaphores;
+
+/**
+ * Created by Dmitry on 07.08.2016.
+ */
+public class Semaphore4 {
+    public void lever() {
+        System.out.println("It works 4");
+    }
+}
